ed05f92ab79540e822d58706f217cf3af9b6061b
Oleg Mikhailovskii <o.mikhaylovskiy@gmail.com>
2023-10-17 11:27:17 +0200

    feature: add option to save space when copying files to use symlinks instead

--- tools/copy-search-results.py
+++ tools/copy-search-results.py
@@ -1,4 +1,5 @@
 import shutil
+import os
 from pathlib import Path
 from typing import List
 
@@ -10,19 +11,26 @@ def get_pdb_codes(prefix: Path) -> List[str]:
     )
 
 
-def main(search_results_dir: Path, output_dir: Path):
+def main(search_results_dir: Path, output_dir: Path, save_disk_space: bool = True):
     pdb_codes = get_pdb_codes(search_results_dir)
 
     for pdb_code in pdb_codes:
         orig_pdb = search_results_dir / "db" / f"pdb{pdb_code}.ent"
         orig_mtz = search_results_dir / "structure_factors" / f"{pdb_code}-sf_P1.mtz"
-
         pdb_dir = output_dir / pdb_code
+        target_pdb = pdb_dir / f"{pdb_code}.pdb"
+        target_mtz = pdb_dir / f"{pdb_code}.mtz"
 
         if orig_mtz.exists() and orig_pdb.exists():
+            if target_pdb.exists() and target_mtz.exists():
+                continue
             pdb_dir.mkdir(exist_ok=True)
-            shutil.copy(orig_pdb, pdb_dir / f"{pdb_code}.pdb")
-            shutil.copy(orig_mtz, pdb_dir / f"{pdb_code}.mtz")
+            if save_disk_space:
+                os.symlink(orig_pdb, target_pdb)
+                os.symlink(orig_mtz, target_mtz)
+            else:
+                shutil.copy(orig_pdb, target_pdb)
+                shutil.copy(orig_mtz, target_mtz)
 
 
 if __name__ == "__main__":
