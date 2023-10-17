e0b1c3b40e88bc160cd4b405f31a8514851008e8
Oleg Mikhailovskii <o.mikhaylovskiy@gmail.com>
2023-10-17 17:04:22 +0200

    add remote runner executioner

---
+++ run_remotely.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import remote_runner
+from remote_runner import LocalSlurmWorker, Pool, Task
+
+
+def main():
+    remote_runner.log_to(".remote-runner.log")
+
+    workers = [
+        LocalSlurmWorker(
+            remote_user_rc="""
+unset PYTHONPATH
+source /opt/amber.sh
+source venv/bin/activate
+""",
+            sbatch_args=[
+                "--cpus-per-task=1",  # cpu here means threads
+                "--gpus=1",
+            ],
+        )
+        for _ in range(4)
+    ]
+
+    tasks = [
+        Task.load(state)
+        for state in sorted(Path("./").glob("data/output/*/*/state.dill"))
+    ]
+    print(len(tasks))
+    Pool(workers).run(tasks)
+
+
+if __name__ == "__main__":
+    main()
